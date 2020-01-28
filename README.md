b259f117a091f6c595d7ce97b5e8068815812416
welbjosh <welbjosh@gmail.com>
2020-01-28 15:12:15 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WEBD6201-W2020-Lesson 3
+
+## demo project for Lesson 3
---
+++ Scripts/app.js
@@ -0,0 +1,41 @@
+/*
+    Student Name  :
+    StudentID     :
+    Date Completed:
+*/
+
+"use strict";
+//IIFE - Immediately Invoked Function Expression
+// mean? -> anonymous self-executing function
+
+let app;
+(function(app){
+
+    // Declare Function Variables here...
+    console.log("%cDeclaring Variables", "color: red;")
+
+    /**
+     * Variable initialization in this function
+     *
+     */
+    function Start()
+    {
+
+
+        Main();
+    }
+
+    /**
+     * Main Program entry point is here
+     *
+     */
+    function Main()
+    {
+
+    }
+
+
+
+    window.addEventListener("load", Start);
+})(app || (app = {}));
+
