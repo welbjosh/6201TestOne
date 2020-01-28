0393810ba894efd314727b93964b1d15396c1170
welbjosh <welbjosh@gmail.com>
2020-01-28 15:39:26 -0500

    Large Button and h3 tag fixed

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,15 +13,31 @@ let app;
 
     // Declare Function Variables here...
     console.log("%cDeclaring Variables", "color: red;")
-
+    let largeButtons;
+    let h3Name;
+    let h3ParentTag;
+    let contentArea;
     /**
      * Variable initialization in this function
      *
      */
     function Start()
     {
+        // Replaces large button text with learn more
+        largeButtons = document.getElementById("largeButton");
+        largeButtons.innerHTML = "Learn More";
+
+        // Creates a h3 tag with my name and student id
+        contentArea = document.getElementById("contentArea")
+
+        h3Name = document.createElement('h3');
+        h3Name.innerHTML = 'Name: Joshua Welby 100663217';
+        contentArea.appendChild(h3Name);
 
 
+        // Hides the h3 paragraph made above
+
+        
         Main();
     }
 
